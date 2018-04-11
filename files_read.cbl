17c3424072b02659d8c3964bdaaa3dac9f335373
Martin <marcindanlowski@gmail.com>
2018-04-11 14:38:56 +0200

    Add files via upload

--- files_read.cbl
+++ files_read.cbl
@@ -6,26 +6,53 @@
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. md-basics-file-read.
+
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-           SELECT TestData ASSIGN TO "test.dat"
-              ORGANISATION IS LINE SEQUENTIAL.
-
+      *> SELECT internal file and ASSIGN it to a external data resource
+           SELECT  StudentsFile ASSIGN TO "students.dat"
+                   ORGANISATION IS LINE SEQUENTIAL.
        DATA DIVISION.
        FILE SECTION.
-       FD TestData.
-       01  Item.
-           88 EOFile   VALUE HIGH-VALUES.
-           02 ItemID   PIC 99.
-           02 UName    PIC A(10).
-           02 UPass    PIC X(10).
-           02 UMail    PIC X(20).
-           02 UData    PIC S9999.
+       FD  StudentsFile.
+       01  StudentDetailsBuffer.
+           02  StuID           PIC 99.
+           02  StuName         PIC A(10).
+           02  StuSurname      PIC A(10).
+           02  StuDob          PIC 9(8).
+           02  StuGender       PIC X.
 
        WORKING-STORAGE SECTION.
+       01  WS-Student.
+           88  EndOfFile               VALUE LOW-VALUES.
+           02  WS-ID                   PIC 99.
+           02  WS-Student-name         PIC A(10).
+           02  WS-Student-surname      PIC A(10).
+           02  WS-Student-dob          PIC 9(8).
+           02  WS-Student-gender       PIC X.
+
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
-            DISPLAY "  DATA READ FROM FILE"
+            DISPLAY "====DATA READ FROM FILE====".
+               PERFORM OPEN-READ
             STOP RUN.
+
+       OPEN-READ.
+            OPEN INPUT StudentsFile.
+               PERFORM UNTIL EndOfFile
+                   READ StudentsFile RECORD
+                       INTO WS-Student
+                       AT END
+                           DISPLAY "====END OF FILE===="
+                           SET EndOfFile TO TRUE
+                       NOT AT END
+                           DISPLAY
+                           WS-ID SPACE WS-Student-name,
+                           WS-Student-surname,
+                           WS-Student-dob SPACE WS-Student-gender
+                   END-READ
+               END-PERFORM.
+            CLOSE StudentsFile.
+
        END PROGRAM md-basics-file-read.
